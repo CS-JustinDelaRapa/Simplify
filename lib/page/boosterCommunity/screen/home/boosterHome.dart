e85ed1edac071c77f9768bd2f5b2120e341a407b
Adrian Alejo <alejoadrian.14@gmail.com>
2021-10-13 14:50:00 +0800

    create

--- lib/page/boosterCommunity/screen/home/boosterHome.dart
+++ lib/page/boosterCommunity/screen/home/boosterHome.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:simplify/page/boosterCommunity/screen/home/crud/add_post_form.dart';
 import 'package:simplify/page/boosterCommunity/service/firebaseHelper.dart';
 
 class BoosterHome extends StatefulWidget {
-  const BoosterHome({ Key? key}) : super(key: key);
+  const BoosterHome({Key? key}) : super(key: key);
 
   @override
   _BoosterHomeState createState() => _BoosterHomeState();
@@ -13,16 +14,30 @@ class _BoosterHomeState extends State<BoosterHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Boosters Community'),
-      ),
-      body: ElevatedButton(
-        child: Text('Sign Out'),
-        onPressed: () {
-          AuthService().signOut();
-        },
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Boosters Community'),
+        ),
+        body: ElevatedButton(
+          child: Text('Sign Out'),
+          onPressed: () {
+            AuthService().signOut();
+          },
+        ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+          child: FloatingActionButton(
+            backgroundColor: Colors.blueGrey[900],
+            child: Icon(
+              Icons.add,
+              size: 30.0,
+            ),
+            onPressed: () async {
+              await Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => AddPostForm()),
+              );
+            },
+          ),
+        ));
   }
 }
---
+++ lib/page/boosterCommunity/screen/home/crud/add_post_form.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:simplify/page/boosterCommunity/service/validator.dart';
+
+class AddPostForm extends StatefulWidget {
+  const AddPostForm({Key? key}) : super(key: key);
+
+  @override
+  _AddPostState createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPostForm> {
+  final _addItemFormKey = GlobalKey<FormState>();
+  bool _isProcessing = false;
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _addItemFormKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 8.0,
+              right: 8.0,
+              bottom: 24.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // eto yung title form field
+                SizedBox(height: 24.0),
+                Text(
+                  'Title',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 22.0,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  controller: _titleController,
+                  keyboardType: TextInputType.text,
+                  validator: (value) => Validator.validateField(
+                    value: value!,
+                  ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Title',
+                    hintStyle: TextStyle(color: Colors.black54),
+                  ),
+                ),
+                //eto yung description form field
+                SizedBox(height: 24.0),
+                Text(
+                  'Description',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 22.0,
+                    letterSpacing: 1,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                TextFormField(
+                  maxLines: 10,
+                  controller: _descriptionController,
+                  keyboardType: TextInputType.text,
+                  validator: (value) => Validator.validateField(
+                    value: value!,
+                  ),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(width: 2, color: Colors.grey.shade300),
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Title',
+                    hintStyle: TextStyle(color: Colors.black54),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          _isProcessing
+              ? Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      Colors.orange,
+                    ),
+                  ),
+                )
+              : Container(
+                  //eto na yung button nagpoprocess ng laman
+                  width: double.maxFinite,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.orange,
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    onPressed: () async {
+                      // if (_addItemFormKey.currentState!.validate()) {
+                      //   setState(() {
+                      //     _isProcessing = true;
+                      //   });
+                      //   await Database.addItem(
+                      //     title: _titleController.text,
+                      //     description: _descriptionController.text,
+                      //   );
+                      //   setState(() {
+                      //     _isProcessing = false;
+                      //   });
+                      //   Navigator.of(context).pop();
+                      // }
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                      child: Text(
+                        'ADD ITEM',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+        ],
+      ),
+    );
+  }
+}
