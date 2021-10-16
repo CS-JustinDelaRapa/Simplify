457dd1819c242ae1399be9d9077de61238ad75b4
Adrian Alejo <alejoadrian.14@gmail.com>
2021-10-16 20:55:38 +0800

    Image icon

---
+++ lib/page/boosterCommunity/screen/home/changeUserIcon/changeUserIcon.dart
@@ -0,0 +1,152 @@
+// import 'package:flutter/material.dart';
+
+// final List<String> iconImageList = [
+//   '001-panda.png',
+//   '002-lion.png',
+//   '003-tiger.png',
+//   '004-bear-1.png',
+//   '005-parrot.png',
+//   '006-rabbit.png',
+//   '007-chameleon.png',
+//   '008-sloth.png',
+//   '009-elk.png',
+//   '010-llama.png',
+//   '011-ant-eater.png',
+//   '012-eagle.png',
+//   '013-crocodile.png',
+//   '014-beaver.png',
+//   '015-hamster.png',
+//   '016-walrus.png',
+//   '017-bear.png',
+//   '018-cheetah.png',
+//   '019-kangaroo.png',
+//   '020-duck.png',
+//   '021-goose.png',
+//   '022-lemur.png',
+//   '023-ostrich.png',
+//   '024-owl.png',
+//   '025-boar.png',
+//   '026-penguin.png',
+//   '027-camel.png',
+//   '028-raccoon.png',
+//   '029-hippo.png',
+//   '030-monkey.png',
+//   '031-meerkat.png',
+//   '032-snake.png',
+//   '033-zebra.png',
+//   '034-donkey.png',
+//   '035-bull.png',
+//   '036-goat-1.png',
+//   '037-goat.png',
+//   '038-horse.png',
+//   '039-wolf.png',
+//   '040-koala.png',
+//   '041-hedgehog.png',
+//   '042-frog.png',
+//   '043-turtle.png',
+//   '044-gorilla.png',
+//   '045-giraffe.png',
+//   '046-deer.png',
+//   '047-rhinoceros.png',
+//   '048-elephant.png',
+//   '049-puma.png',
+//   '050-fox.png'
+// ];
+
+// class ChangeUserIcon extends StatefulWidget {
+//   const ChangeUserIcon({Key? key}) : super(key: key);
+
+//   @override
+//   _ChangeUserIconState createState() => _ChangeUserIconState();
+// }
+
+// class _ChangeUserIconState extends State<ChangeUserIcon> {
+//   late String userIcon;
+
+//   @override
+//   void initState() {
+//     userIcon = '001-panda.png';
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final size = MediaQuery.of(context).size;
+//     return Stack(
+//       children: <Widget>[
+//         SimpleDialog(
+//           shape: RoundedRectangleBorder(
+//               borderRadius: BorderRadius.all(Radius.circular(16.0))),
+//           contentPadding: EdgeInsets.zero,
+//           children: <Widget>[
+//             Container(
+//               width: size.width,
+//               height: size.height - size.height * 0.12,
+//               child: Container(
+//                 child: Padding(
+//                   padding: EdgeInsets.all(10),
+//                   child: GridView.count(
+//                       crossAxisCount: 4,
+//                       crossAxisSpacing: 16,
+//                       padding: EdgeInsets.all(8),
+//                       physics: ScrollPhysics(),
+//                       shrinkWrap: true,
+//                       children: iconImageList.map(_makeGridTile).toList()),
+//                 ),
+//               ),
+//             )
+//           ],
+//         ),
+//         Positioned(
+//           bottom: 10,
+//           right: 10,
+//           child: Row(
+//             children: <Widget>[
+//               Padding(
+//                 padding: const EdgeInsets.only(left: 2.0, right: 2.0),
+//                 child: ClipOval(
+//                   child: Container(
+//                     color: Colors.blue,
+//                     height: 60.0, // height of the button
+//                     width: 60.0, // width of the button
+//                     child: RaisedButton(
+//                       elevation: 8.0,
+//                       color: Colors.black,
+//                       onPressed: () {
+//                         Navigator.pop(context, userIcon);
+//                       },
+//                       child: Icon(
+//                         Icons.close,
+//                         size: 32,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               )
+//             ],
+//           ),
+//         )
+//       ],
+//     );
+//   }
+
+//   Widget _makeGridTile(String userIconPath) {
+//     return GridTile(
+//       child: GestureDetector(
+//           onTap: () {
+//             setState(() {
+//               userIcon = userIconPath;
+//             });
+//           },
+//           child: Container(
+//               decoration: BoxDecoration(
+//                 color: userIconPath == userIcon ? Colors.yellow : Colors.white,
+//                 border: userIconPath == userIcon
+//                     ? Border.all(width: 3, color: Colors.red)
+//                     : null,
+//               ),
+//               child: Image.asset('images/$userIconPath'))), //image,
+//     );
+//   }
+// }
--- lib/page/boosterCommunity/screen/home/profile.dart
+++ lib/page/boosterCommunity/screen/home/profile.dart
@@ -2,23 +2,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:simplify/page/boosterCommunity/model/myuser.dart';
+import 'package:simplify/page/boosterCommunity/screen/home/changeUserIcon/changeUserIcon.dart';
 import 'package:simplify/page/boosterCommunity/service/firebaseHelper.dart';
 
 class Profile extends StatefulWidget {
-
-  const Profile({ Key? key }) : super(key: key);
+  const Profile({Key? key}) : super(key: key);
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
-final CollectionReference userCollection = FirebaseFirestore.instance.collection('users');
-final FirebaseAuth _auth = FirebaseAuth.instance;
-late String userId;
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('users');
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  late String userId;
 
 //comment
 
-@override
+  @override
   void initState() {
     userId = _auth.currentUser!.uid.toString();
     print('CEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEB ' + userId.toString());
@@ -27,68 +28,79 @@ late String userId;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: FutureBuilder<DocumentSnapshot>(
-        future: userCollection.doc(userId).get(),
-        builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text("Something went wrong");
-        }          
-        if (snapshot.hasData && !snapshot.data!.exists) {
-          return Text("Document does not exist");
-        }          
-          if(snapshot.connectionState == ConnectionState.done){     
-          Map<String, dynamic> data = snapshot.data!.data() as Map<String, dynamic>;
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[50],
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                        BoxShadow(
-                            color: Colors.black26,
-                            blurRadius: 4,
-                            offset: Offset(2, 2)),
-                      ],
-                  ),
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          height: 80,
-                          width: 80,
-                        decoration: BoxDecoration(
-                          color: Colors.grey,
-                          shape: BoxShape.circle
-                        ),
-                        child: Center(
-                          child: Icon(Icons.person, size: 35)
-                          )
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text('${data['first-name']} ${data['last-name']}', textAlign: TextAlign.left,),
-                          Text('${data['school']}'),                          
+          future: userCollection.doc(userId).get(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text("Something went wrong");
+            }
+            if (snapshot.hasData && !snapshot.data!.exists) {
+              return Text("Document does not exist");
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              Map<String, dynamic> data =
+                  snapshot.data!.data() as Map<String, dynamic>;
+              return Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[50],
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 4,
+                              offset: Offset(2, 2)),
                         ],
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
-          );
-                    }else{
-                      return Center(child: CircularProgressIndicator());
-                    }
-        }
-      ),
+                      ),
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Column(
+                              children: [
+                                Container(
+                                    height: 60,
+                                    width: 60,
+                                    // decoration: BoxDecoration(
+                                    //     color: Colors.grey, shape: BoxShape.circle),
+                                    child: Image.asset(
+                                        'assets/images/001-panda.png')),
+                                // GestureDetector(
+                                //   onTap: () {
+                                //     showDialog(
+                                //       context: context,
+                                //       builder: (context) => ChangeUserIcon(),
+                                //       barrierDismissible: true,
+                                //     );
+                                //   },
+                                // )
+                              ],
+                            ),
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                '${data['first-name']} ${data['last-name']}',
+                                textAlign: TextAlign.left,
+                              ),
+                              Text('${data['school']}'),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
     );
   }
 }
