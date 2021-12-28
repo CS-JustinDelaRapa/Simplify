8fac3587f22e7ee27557c19789d7257893a59341
Adrian Alejo <alejoadrian.14@gmail.com>
2021-12-28 18:49:22 +0800

    pushing

--- lib/model/grade_tracker/gradeFactor.dart
+++ lib/model/grade_tracker/gradeFactor.dart
@@ -16,7 +16,6 @@ class TblFactorField {
   static final String factorGrade = 'factor_Grade';
   static final String factorPercentage = 'factor_percentage';
   static final String fkCourse = 'fk_course';
-
 }
 
 class Factor {
@@ -28,34 +27,34 @@ class Factor {
   final double factorPercentage;
   final int fkCourse;
 //const will just let a function to just read the value of our Diary class current value (context)
-  const Factor({
-    //initializing our parameter, title = this, this refer's to a current method value.
-    this.id,
-    required this.factorName,
-    required this.factorGrade,
-    required this.factorPercentage,
-    required this.fkCourse
-  });
+  const Factor(
+      {
+      //initializing our parameter, title = this, this refer's to a current method value.
+      this.id,
+      required this.factorName,
+      required this.factorGrade,
+      required this.factorPercentage,
+      required this.fkCourse});
 
   Map<String, Object?> toJson() => {
         //Key: Value, Column Field: Value
         TblFactorField.id: id,
         TblFactorField.factorName: factorName,
+        TblFactorField.factorPercentage: factorPercentage,
         TblFactorField.factorGrade: factorGrade,
-        TblFactorField.factorGrade: factorPercentage,
-        TblFactorField.factorGrade: fkCourse,
+        TblFactorField.fkCourse: fkCourse,
         //INSERT INTO tbl_Diary('_id',..) Values ('null','Tiltle'..)
       };
 
   //explain this well, Diary returnID is a constuctor
-  Factor returnID(
-          { //edited here
-          int? parameterID,
-          String? factorName,
-          double? factorGrade,
-          double? factorPercentage,
-          int? fkCourse,
-          }) =>
+  Factor returnID({
+    //edited here
+    int? parameterID,
+    String? factorName,
+    double? factorGrade,
+    double? factorPercentage,
+    int? fkCourse,
+  }) =>
       Factor(
           //?? = Null-Coalsing Operator, meaning, if ParameterID is null then use the value of this(context) id, else it will return the value of ParameterID
           //if parameterID is null this line 41 will cause then an error because a ?? operator right operand must not be null.
@@ -63,14 +62,13 @@ class Factor {
           factorName: factorName ?? this.factorName,
           factorGrade: factorGrade ?? this.factorGrade,
           factorPercentage: factorPercentage ?? this.factorPercentage,
-          fkCourse: fkCourse ?? this.fkCourse
-          ); //until here
+          fkCourse: fkCourse ?? this.fkCourse); //until here
 
   static Factor fromJson(Map<String, Object?> fromSQL) => Factor(
-      id: fromSQL[TblFactorField.id] as int?,
-      factorName: fromSQL[TblFactorField.factorName] as String,
-      factorGrade: fromSQL[TblFactorField.factorGrade] as double,
-      factorPercentage: fromSQL[TblFactorField.factorGrade] as double,
-      fkCourse: fromSQL[TblFactorField.fkCourse] as int,
+        id: fromSQL[TblFactorField.id] as int?,
+        factorName: fromSQL[TblFactorField.factorName] as String,
+        factorGrade: fromSQL[TblFactorField.factorGrade] as double,
+        factorPercentage: fromSQL[TblFactorField.factorGrade] as double,
+        fkCourse: fromSQL[TblFactorField.fkCourse] as int,
       );
 }
