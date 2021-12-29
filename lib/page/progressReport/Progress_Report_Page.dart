97ea09246d3a730d6e6370ce738bd17cc0f7d7fb
Adrian Alejo <alejoadrian.14@gmail.com>
2021-12-29 18:35:07 +0800

    progress report

--- lib/page/progressReport/Progress_Report_Page.dart
+++ lib/page/progressReport/Progress_Report_Page.dart
@@ -79,7 +79,6 @@ class _ProgressReportPageState extends State<ProgressReportPage> {
           ),
           backgroundColor: Colors.transparent,
           elevation: 0.0,
-          actions: [refreshButton()],
         ),
         body: isLoading
             ? Center(child: CircularProgressIndicator())
@@ -89,77 +88,77 @@ class _ProgressReportPageState extends State<ProgressReportPage> {
                   padding: EdgeInsets.all(10),
                   width: 380,
                   height: 380,
-                  child: Center(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        finishedPercentage == 100.00
-                            ? ConfettiWidget(
-                                confettiController: _controllerCenter,
-                                blastDirectionality:
-                                    BlastDirectionality.explosive,
-                                particleDrag: 0.05,
-                                emissionFrequency: 0.05,
-                                numberOfParticles: 50,
-                                gravity: 0.05,
-                                shouldLoop: false,
-                                colors: const [
-                                  Colors.green,
-                                  Colors.blue,
-                                  Colors.pink,
-                                  Colors.orange,
-                                  Colors.purple
-                                ], // manually specify the colors to be used
-                              )
-                            : Container(),
-                        Text(
-                          finishedPercentage.toStringAsFixed(2) + "%",
-                          style: TextStyle(
-                              fontSize: 70,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
+                  child: this.taskContent.isEmpty
+                      ? Center(
+                          child: Text(
+                            "No task inputted",
+                            style: TextStyle(
+                                fontSize: 25,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
+                      : Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              finishedPercentage == 100.00
+                                  ? ConfettiWidget(
+                                      confettiController: _controllerCenter,
+                                      blastDirectionality:
+                                          BlastDirectionality.explosive,
+                                      particleDrag: 0.05,
+                                      emissionFrequency: 0.05,
+                                      numberOfParticles: 50,
+                                      gravity: 0.05,
+                                      shouldLoop: false,
+                                      colors: const [
+                                        Colors.green,
+                                        Colors.blue,
+                                        Colors.pink,
+                                        Colors.orange,
+                                        Colors.purple
+                                      ], // manually specify the colors to be used
+                                    )
+                                  : Container(),
+                              Text(
+                                finishedPercentage.toStringAsFixed(2) + "%",
+                                style: TextStyle(
+                                    fontSize: 70,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Text(
+                                "Task completed",
+                                style: TextStyle(
+                                    fontSize: 25,
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(70, 10, 10, 10),
+                                  child: LinearPercentIndicator(
+                                    //leaner progress bar
+                                    width: 210, //width for progress bar
+                                    animation:
+                                        true, //animation to show progress at first
+                                    animationDuration: 1000,
+                                    addAutomaticKeepAlive: false,
+                                    lineHeight: 25.0, //height of progress bar
+                                    percent: finishedPercentage /
+                                        100, // 30/100 = 0.3//make round cap at start and end both
+                                    progressColor: Colors
+                                        .black, //percentage progress bar color
+                                    backgroundColor: Colors
+                                        .grey, //background progressbar color
+                                  )),
+                            ],
+                          ),
                         ),
-                        Text(
-                          "Task completed",
-                          style: TextStyle(
-                              fontSize: 25,
-                              color: Colors.black,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        Container(
-                            padding: EdgeInsets.fromLTRB(70, 10, 10, 10),
-                            child: LinearPercentIndicator(
-                              //leaner progress bar
-                              width: 210, //width for progress bar
-                              animation:
-                                  true, //animation to show progress at first
-                              animationDuration: 1000,
-                              lineHeight: 25.0, //height of progress bar
-                              percent: finishedPercentage /
-                                  100, // 30/100 = 0.3//make round cap at start and end both
-                              progressColor:
-                                  Colors.black, //percentage progress bar color
-                              backgroundColor:
-                                  Colors.grey, //background progressbar color
-                            )),
-                        // MaterialButton(
-                        //   onPressed: () {
-                        //     //do whatever you want
-                        //   },
-                        //   child: Text("Mark Attendance"),
-                        // ),
-                        // MaterialButton(
-                        //   onPressed: () {
-                        //     //do whatever you want
-                        //   },
-                        //   child: Text("Mark Attendance"),
-                        // ),
-                      ],
-                    ),
-                  ),
                   decoration: BoxDecoration(
                     border: Border.all(width: 2),
                     borderRadius: BorderRadius.all(
@@ -169,105 +168,8 @@ class _ProgressReportPageState extends State<ProgressReportPage> {
                   ),
                 ),
               ),
-        // Container(
-        //     child: Column(
-        //       children: <Widget>[
-        //         Expanded(
-        //           flex: 4,
-        //           child: PieChart(
-        //             PieChartData(
-        //               sectionsSpace: 0,
-        //               centerSpaceRadius: 0,
-        //               sections: <PieChartSectionData>[
-        //                 PieChartSectionData(
-        //                     color: Colors.black,
-        //                     value: doneTask,
-        //                     title: finishedPercentage.toStringAsFixed(2) +
-        //                         "%",
-        //                     radius: 150,
-        //                     titleStyle: TextStyle(
-        //                       fontSize: 24,
-        //                       fontWeight: FontWeight.bold,
-        //                       color: Colors.white,
-        //                     )),
-        //                 PieChartSectionData(
-        //                     color: Colors.white,
-        //                     value: totalTask - doneTask,
-        //                     title:
-        //                         unfinishedPercentage.toStringAsFixed(2) +
-        //                             "%",
-        //                     radius: 150,
-        //                     titleStyle: TextStyle(
-        //                       fontSize: 24,
-        //                       fontWeight: FontWeight.bold,
-        //                       color: Colors.black,
-        //                     )),
-        //               ],
-        //             ),
-        //           ),
-        //         ),
-        //         Expanded(
-        //           flex: 2,
-        //           child: Padding(
-        //             padding: const EdgeInsets.all(16.0),
-        //             child: Row(
-        //               mainAxisAlignment: MainAxisAlignment.center,
-        //               children: [
-        //                 Column(
-        //                   children: <Widget>[
-        //                     Row(
-        //                       children: [
-        //                         Container(
-        //                           width: 16,
-        //                           height: 16,
-        //                           decoration: BoxDecoration(
-        //                             shape: BoxShape.circle,
-        //                             color: Colors.black,
-        //                           ),
-        //                         ),
-        //                         SizedBox(
-        //                           width: 10,
-        //                         ),
-        //                         Text('Finished task',
-        //                             style: TextStyle(
-        //                                 fontSize: 16,
-        //                                 fontWeight: FontWeight.bold,
-        //                                 color: Colors.black87)),
-        //                       ],
-        //                     ),
-        //                     Row(
-        //                       children: [
-        //                         Container(
-        //                           width: 16,
-        //                           height: 16,
-        //                           decoration: BoxDecoration(
-        //                             shape: BoxShape.circle,
-        //                             color: Colors.white,
-        //                           ),
-        //                         ),
-        //                         SizedBox(
-        //                           width: 10,
-        //                         ),
-        //                         Text('Unfinished task',
-        //                             style: TextStyle(
-        //                                 fontSize: 16,
-        //                                 fontWeight: FontWeight.bold,
-        //                                 color: Colors.black87)),
-        //                       ],
-        //                     )
-        //                   ],
-        //                 )
-        //               ],
-        //             ),
-        //           ),
-        //         ),
-        //       ],
-        //     ),
-        //   )
       ),
     );
   }
 
-  Widget refreshButton() =>
-      IconButton(onPressed: refreshState, icon: Icon(Icons.refresh_rounded));
 }
